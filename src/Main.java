15d61f60bc5cfe74c188f666e584ea4998da331f
Sumeyya <sumeyya_selvi@hotmail.com>
2023-07-15 15:51:31 +0300

    Dizinin frekansını bulan program

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = new int []{10,20,20,10,20,5,20};
+        int[] fr = new int[arr.length];
+        int visited = -1;
+        for( int i =0;i<arr.length;i++){
+            int count =1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    count ++;
+                    fr[j]=visited;
+                }
+            }
+            if(fr[i] != visited)
+                fr[i]= count;
+
+        }
+        System.out.println("__________________");
+        System.out.println("Element | Frequency");
+        System.out.println("____________________");
+        for(int i=0;i<fr.length;i++){
+            if(fr[i]!=visited)
+                System.out.println("    " + arr[i] + "|" + fr[i]);
+        }
+        System.out.println("_____________________");
+
+    }
+
+
+    }
